6d31e49bfc839d8d7501bd82339c316a3a348189
YosefLehrer <yolehrer@gmail.com>
2019-11-27 02:07:26 -0500

    Twitter feed working and started on GitHub feed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Twitter from "./components/Twitter";
+import Github from "./components/Github";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Yosef Lehrer</h1>
+      <h4>Site Under Construction</h4>
+      <Twitter />
+      <Github />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>LinkedIn</div>
+      <div>GitHub</div>
+      <div>Medium</div>
+      <div>Twitter</div>
+    </div>
+  );
+};
+
+export default Footer;
